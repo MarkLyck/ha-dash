d4e0d439be17a276cf859572144d989f201fd32c
Mark Lyck <mark.lyck@gmail.com>
2023-03-10 00:24:27 +0100

    :sparkles: add fontawesome

--- src/components/status/status.tsx
+++ src/components/status/status.tsx
@@ -1,5 +1,7 @@
 import type { Entity } from "@/lib/types/entities";
 import { cva } from "class-variance-authority";
+import { FontAwesomeIcon, getEntityIcon } from "@/utils/icon";
+import { getEntityColor } from "@/utils/color";
 
 const iconContainer = cva(
   [
@@ -12,27 +14,35 @@ const iconContainer = cva(
     "justify-center",
     "items-center",
     "mb-2",
+    "bg-opacity-25",
   ],
   {
     variants: {
       type: {
-        lock: ["bg-blue-500"],
-        camera: ["bg-green-500"],
+        lock: "bg-blue-500 text-blue-600",
+        light: "bg-yellow-500 text-yellow-600",
+        camera: "bg-red-500 text-red-600",
       },
     },
   }
 );
 
 type StatusProps = {
-  entity: Entity;
+  entity?: Entity;
 };
 
 export const Status = ({ entity }: StatusProps) => {
+  if (!entity) return null;
+  const entityColor = getEntityColor(entity);
+  console.log("🔈 ~ entityColor:", entityColor);
+
   return (
     <div className="flex flex-col items-center">
-      <div className={iconContainer({ type: entity.domain })}>icon</div>
+      <div className={iconContainer({ type: entity.domain })}>
+        <FontAwesomeIcon icon={getEntityIcon(entity)} />
+      </div>
       <h4 className="font-semibold">{entity.name}</h4>
-      <span>{entity.state}</span>
+      <span className="opacity-50">{entity.state}</span>
     </div>
   );
 };
--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -1,6 +1,28 @@
+import {
+  faCameraCctv,
+  faQuestion,
+  faLock,
+  faLockOpen,
+  faLightbulb,
+  faLightbulbOn,
+} from "@fortawesome/pro-duotone-svg-icons";
+
+import type { Icon } from "@fortawesome/fontawesome-svg-core";
+export { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import type { Entity } from "@/lib/types/entities";
 
-export const getEntityIcon = (entity: Entity) => {
-  console.log("🔈 ~ entity:", entity);
-  return "icon";
+export const getEntityIcon = (entity: Entity): Icon => {
+  let icon: unknown = faQuestion;
+  if (entity.domain === "lock") {
+    icon = faLock;
+    if (entity.state === "unlocked") icon = faLockOpen;
+  } else if (entity.domain === "light") {
+    icon = faLightbulb;
+    if (entity.state === "on") icon = faLightbulbOn;
+  } else if (entity.domain === "camera") {
+    icon = faCameraCctv;
+  }
+
+  return icon as Icon;
 };
