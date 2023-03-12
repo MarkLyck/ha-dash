bb049b0789addb146a9cab618ca15d948c2bc486
Mark Lyck <mark.lyck@gmail.com>
2023-03-12 23:08:50 +0100

    :sparkles: light card work

--- src/components/device/light/LightCard.stories.tsx
+++ src/components/device/light/LightCard.stories.tsx
@@ -10,6 +10,76 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof LightCard>
 
+export const Demo: Story = {
+  render: () => (
+    <div className="flex flex-wrap gap-4">
+      <LightCard
+        name="Outdoor bulb"
+        icon={['fad', 'lightbulb-on']}
+        isOn
+        color="yellow"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Living room light"
+        icon={['fad', 'light-ceiling']}
+        isOn={false}
+        color="yellow"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Desk lamp"
+        icon={['fad', 'lamp-desk']}
+        isOn
+        color="purple"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Floor lamp"
+        icon={['fad', 'lamp-floor']}
+        isOn={false}
+        color="#ffffff"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Sofa lamp"
+        icon={['fad', 'lamp']}
+        isOn
+        color="lightpink"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Holiday lights"
+        icon={['fad', 'lights-holiday']}
+        isOn
+        color="red"
+        setState={() => {
+          //
+        }}
+      />
+      <LightCard
+        name="Christmas tree"
+        icon={['fad', 'tree-christmas']}
+        isOn
+        color="green"
+        setState={() => {
+          //
+        }}
+      />
+    </div>
+  ),
+}
+
 export const OutdoorBulb: Story = {
   args: {
     name: 'Outdoor bulb',
@@ -25,7 +95,7 @@ export const OutdoorBulb: Story = {
 export const DeskLamp: Story = {
   args: {
     name: 'Desk lamp',
-    icon: ['fad', 'lamp'],
+    icon: ['fad', 'lamp-desk'],
     isOn: true,
     color: 'yellow',
     setState: () => {
