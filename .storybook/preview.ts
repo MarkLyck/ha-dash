ebf23baaddb72f4bb3430d4455cab996551c9262
Mark Lyck <mark.lyck@gmail.com>
2024-05-19 22:22:31 -0400

    :bug: Fix storybook errors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import type { Preview } from '@storybook/react'
 
-import { WithTheme } from './decorators/withTheme'
+// import { WithTheme } from './decorators/withTheme'
 
 import 'tailwindcss/tailwind.css'
 import '@/lib/icons'
@@ -9,7 +9,7 @@ import '@/styles/global.css'
 import './index.css'
 
 const preview: Preview = {
-  decorators: [WithTheme],
+  // decorators: [WithTheme],
   parameters: {
     viewport: {
       viewports: INITIAL_VIEWPORTS,
