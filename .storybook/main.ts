b741a04092fb11faa19699e19526c26db7d6e5f8
Mark Lyck <mark.lyck@gmail.com>
2024-05-20 00:00:27 -0400

    :bug: Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,27 +10,5 @@ const config: StorybookConfig = {
   ],
   framework: '@storybook/nextjs',
   docs: {},
-  webpackFinal: async (config) => {
-    if (config.module?.rules) {
-      // This modifies the existing image rule to exclude .svg files
-      // since you want to handle those files with @svgr/webpack
-      const imageRule = config.module.rules.find((rule) =>
-        // @ts-expect-error - "test" doesn't exist on rule
-        rule.test?.test('.svg'),
-      )
-      if (imageRule) {
-        // @ts-expect-error - "test" doesn't exist on rule
-        imageRule.exclude = /\.svg$/
-      }
-
-      // Configure .svg files to be loaded with @svgr/webpack
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-    }
-
-    return config
-  },
 }
 export default config
