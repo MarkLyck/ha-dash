941263015109c1de25ceff49ff443c3b9fb8154f
Mark Lyck <mark.lyck@gmail.com>
2024-05-19 21:43:53 -0400

    :fire: Remove eslint

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 'use client'
 
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
