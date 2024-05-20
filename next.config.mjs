97d2e528db80aff129227062be41f5bc61bae56e
Mark Lyck <mark.lyck@gmail.com>
2024-05-19 23:41:32 -0400

    :sparkles: Add react-compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
   images: {
     remotePatterns: [
       {
@@ -21,40 +19,6 @@ const config = {
         pathname: '/**',
       },
     ],
-  },
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-        },
-        // {
-        //   loader: 'file-loader',
-        //   options: {
-        //     name: 'static/[path][name].[ext]',
-        //   },
-        // },
-      ],
-      type: 'javascript/auto',
-      issuer: {
-        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-      },
-    })
-
-    return config
   },
 }
 
