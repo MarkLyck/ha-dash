019172eefbd7f23c6a6b7bf33c8498e2f07b84ef
Mark Lyck <mark.lyck@gmail.com>
2024-05-20 22:41:29 -0400

    :bug: Fix brightness slider slownes

--- src/components/ui/brightnessSlider.tsx
+++ src/components/ui/brightnessSlider.tsx
@@ -15,7 +15,14 @@ type BrightnessSliderProps = React.ComponentPropsWithoutRef<
 export const BrightnessSlider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   BrightnessSliderProps
->(({ className, color, ...props }, ref) => {
+>(({ className, color, value, ...props }, ref) => {
+  const [sliderValue, setSliderValue] = React.useState(value)
+
+  React.useEffect(() => {
+    // @ts-expect-error - undefined
+    props.onValueChange?.(sliderValue)
+  }, [sliderValue])
+
   return (
     <SliderPrimitive.Root
       orientation="vertical"
@@ -24,7 +31,9 @@ export const BrightnessSlider = React.forwardRef<
         'relative flex w-[80px] touch-none select-none items-center',
         className,
       )}
+      value={sliderValue}
       {...props}
+      onValueChange={(value) => setSliderValue(value)}
     >
       <SliderPrimitive.Track className="relative h-60 w-full grow overflow-hidden rounded-[20px] border-2 border-border bg-white hover:cursor-pointer dark:bg-black">
         <SliderPrimitive.Range
@@ -34,7 +43,7 @@ export const BrightnessSlider = React.forwardRef<
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb className="relative mx-2 mt-4 block h-[6px] w-[62px] cursor-grab rounded border border-none bg-black transition focus:outline-none">
         <div className="-right-6 -translate-y-1/2 absolute top-1/2 translate-x-1/2 rounded-full bg-blue-500 px-2 py-1 text-xs">
-          {props.value}%
+          {sliderValue}%
         </div>
       </SliderPrimitive.Thumb>
     </SliderPrimitive.Root>
