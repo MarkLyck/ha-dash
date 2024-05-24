9925a369acacafb417d61c1dacd4cbc064d2d877
Mark Lyck <mark.lyck@gmail.com>
2024-05-24 17:16:03 -0400

    :sparkles: Add lights card

--- src/components/dashboard/areas/lights.tsx
+++ src/components/dashboard/areas/lights.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import useStore from '@/lib/useStore'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { callService } from '@/lib/hass'
 
 import type { HassEntity } from 'home-assistant-js-websocket'
 
@@ -20,14 +22,32 @@ export const LightsControl = ({ entityIds }: LightsControlProps) => {
 
   const lightsOn = lightEntities.filter((entity) => entity.state === 'on')
   const areAllLightsOn = lightEntities.length === lightsOn.length
+  const areAllLightsOff = lightsOn.length === 0
+
+  const toggleLights = async (service: 'turn_on' | 'turn_off') => {
+    await callService({
+      domain: 'light',
+      service,
+      service_data: {},
+      target: { entity_id: lightEntities.map((entity) => entity.entity_id) },
+    })
+  }
 
   return (
     <Card className="inline-flex flex-col items-center justify-center gap-4 rounded-lg border-border/20 bg-black/20 p-4 text-white backdrop-blur-lg">
-      {areAllLightsOn
-        ? `${lightsOn.length} all lights on`
-        : `${lightsOn.length}/${lightEntities.length} lights on`}
-      <Button>
-        <FontAwesomeIcon icon={['fast', 'power-off']} />
+      <FontAwesomeIcon icon={['fas', 'lightbulb']} />
+      {areAllLightsOn ? `${lightsOn.length} lights on` : null}
+      {areAllLightsOff ? 'Lights off' : null}
+      {!areAllLightsOn && !areAllLightsOff
+        ? `${lightsOn.length}/${lightEntities.length} lights on`
+        : null}
+      <Button
+        className="w-full"
+        onClick={() =>
+          toggleLights(lightsOn.length > 0 ? 'turn_off' : 'turn_on')
+        }
+      >
+        <FontAwesomeIcon icon={['fas', 'power-off']} />
       </Button>
     </Card>
   )
