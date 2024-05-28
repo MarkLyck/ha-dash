d4ce8028cd14259d4cb534c0edfdce4dd81fec19
Mark Lyck <mark.lyck@gmail.com>
2024-05-27 20:48:46 -0400

    :sparkles: Add tesla images

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+'use client'
+
 import { AreasCard } from '@/components/dashboard/areas'
 import { Climate } from '@/components/device/climate'
+import useStore from '@/lib/useStore'
 
 const Home = () => {
+  const entities = useStore((s) => s.entities)
+
+  const climateEntityId = Object.keys(entities).find((entityId) =>
+    entityId.startsWith('climate'),
+  )
+  const vacuumEntityId = Object.keys(entities).find((entityId) =>
+    entityId.startsWith('vacuum'),
+  )
+  console.log('🔈 ~ vacuumEntityId:', vacuumEntityId)
+
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <AreasCard className="col-span-4" />
-      <Climate />
+      <Climate entityId={climateEntityId} />
     </div>
   )
 }
--- src/components/device/thermostat/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ThermostatCard, type ThermostatCardProps } from './Card'
-
-export const Thermostat = (props: ThermostatCardProps) => {
-  return <ThermostatCard {...props} />
-}
