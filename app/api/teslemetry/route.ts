a0025a9b7be790d69bab6b4d7ba5982a3ee3f2f0
Mark Lyck <mark.lyck@gmail.com>
2024-05-28 23:45:34 -0400

    :sparkles: Change data structure for teslemetry

--- app/api/teslemetry/route.ts
+++ app/api/teslemetry/route.ts
@@ -4,10 +4,16 @@ import { NextResponse } from 'next/server'
 // example data
 // {"data":{"Locked":"true","Location":{"latitude":39.124548,"longitude":-77.165705}},"createdAt":"2024-05-29T02:10:50.714429888Z","vin":"7SAYGDEEXPF836682"}
 
+// webhook POST for Teslemetry
 export async function POST(request: Request) {
   try {
     const json = await request.json()
-    await kv.set('tesla_state', json)
+    await kv.set('tesla_state', {
+      locked: json.data.Locked,
+      location: json.data.Location,
+      createdAt: json.createdAt,
+      vin: json.vin,
+    })
   } catch (error: any) {
     return new Response(`Webhook error: ${error.message}`, {
       status: 400,
@@ -20,7 +26,6 @@ export async function POST(request: Request) {
 export async function GET() {
   try {
     const teslaState = await kv.get('tesla_state')
-    console.log('🔈 ~ GET tesla_state:', teslaState)
     return NextResponse.json(teslaState)
   } catch (error: any) {
     return new Response(`error: ${error.message}`, {
