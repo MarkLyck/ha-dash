052645355afbf6bb9a4d475e85cccce8ca6422e4
Mark Lyck <mark.lyck@gmail.com>
2024-05-30 12:10:22 -0400

    :sparkles: Dynamic card size

--- src/components/device/car/TeslaCard.tsx
+++ src/components/device/car/TeslaCard.tsx
@@ -8,6 +8,7 @@ import type { Location } from './types'
 import { Button } from '@/components/ui/button'
 import { LockIcon, LockOpenIcon } from '@/assets/icons'
 import { cn } from '@/lib/utils'
+import { useEffect, useRef, useState } from 'react'
 
 type TeslaCardProps = {
   className?: string
@@ -16,9 +17,40 @@ type TeslaCardProps = {
 }
 
 export const TeslaCard = ({ className, location, locked }: TeslaCardProps) => {
+  const cardRef = useRef<HTMLDivElement>(null)
+  const [size, setSize] = useState<{
+    width: number | undefined
+    height: number | undefined
+  }>({
+    width: 0,
+    height: 0,
+  })
+  console.log('🔈 ~ size:', size)
+
+  useEffect(() => {
+    // when the component gets mounted
+    setSize({
+      width: cardRef.current?.offsetWidth,
+      height: cardRef.current?.offsetHeight,
+    })
+    // to handle page resize
+    const getSize = () => {
+      setSize({
+        width: cardRef.current?.offsetWidth,
+        height: cardRef.current?.offsetHeight,
+      })
+    }
+    window.addEventListener('resize', getSize)
+    // remove the event listener before the component gets unmounted
+    return () => window.removeEventListener('resize', getSize)
+  }, [cardRef])
+
   return (
     <Card className={cn('overflow-hidden', className)}>
-      <CardContent className="relative flex flex-col items-center justify-center gap-2 bg-[#040405] p-4">
+      <CardContent
+        ref={cardRef}
+        className="relative flex flex-col items-center justify-center gap-2 bg-[#040405] p-4"
+      >
         <div className="z-20 mt-[100px]">
           <Image
             src="/images/tesla/model_y_front.png"
@@ -34,7 +66,7 @@ export const TeslaCard = ({ className, location, locked }: TeslaCardProps) => {
         </div>
         {location ? (
           <div className="absolute top-0 right-0 left-0 z-1 overflow-hidden rounded-lg">
-            <div className="absolute right-0 bottom-0 left-0 z-10 h-[100px] w-full bg-gradient-to-t from-black to-transparent" />
+            <div className="pointer-events-none absolute right-0 bottom-0 left-0 z-10 h-[100px] w-full bg-gradient-to-t from-black to-transparent" />
             <MapBox
               mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
               initialViewState={{
@@ -43,7 +75,7 @@ export const TeslaCard = ({ className, location, locked }: TeslaCardProps) => {
                 zoom: 14,
               }}
               // TODO make it fit the card
-              style={{ width: 400, height: 160 }}
+              style={{ width: size.width, height: 180 }}
               mapStyle="mapbox://styles/marklyck/clwspy9v505hd01nxhp06ahd0"
             >
               <Marker
