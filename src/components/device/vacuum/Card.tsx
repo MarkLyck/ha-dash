8508ee4f423aa8a64485057b6bc93f568f74909d
Mark Lyck <mark.lyck@gmail.com>
2024-07-02 08:26:25 -0400

    :sparkles: Progress

--- src/components/device/vacuum/Card.tsx
+++ src/components/device/vacuum/Card.tsx
@@ -1,59 +1,37 @@
-import type { IconProp } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
 
-import { DeviceCard } from '@/components/ui/deviceCard'
-import {
-  QuickActionButton,
-  StateActionButton,
-} from '@/components/ui/quickActionButton'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { TbPower } from 'react-icons/tb'
 
-export interface VacuumCardProps {
-  name: string
-  mode: 'off' | 'cleaning' | 'returning'
-  batteryPercentage?: number
-  isCharging?: boolean
-  icon: IconProp
-  setState: (state: boolean) => void
+export type VacuumCardProps = {
+  className?: string
+  startVacuum: () => void
 }
 
-export const VacuumCard = ({
-  name,
-  icon,
-  mode,
-  batteryPercentage,
-  isCharging,
-  setState,
-}: VacuumCardProps) => {
-  const isActive = mode !== 'off'
-
+export const VacuumCard = ({ className, startVacuum }: VacuumCardProps) => {
   return (
-    <DeviceCard
-      isActive={isActive}
-      name={name}
-      status={mode}
-      batteryPercentage={batteryPercentage}
-      isCharging={isCharging}
-      action={
-        <StateActionButton isActive={isActive}>
-          <FontAwesomeIcon icon={['far', isActive ? 'home' : 'play']} />
-        </StateActionButton>
-      }
-      Icon={(props) => <FontAwesomeIcon icon={icon} {...props} />}
-      setIsActive={setState}
-    >
-      {isActive ? (
-        <div className="flex justify-between gap-1">
-          <QuickActionButton>
-            <FontAwesomeIcon icon={['fas', 'pause']} />
-          </QuickActionButton>
-          <QuickActionButton>
-            <FontAwesomeIcon icon={['far', 'fan']} />
-          </QuickActionButton>
-          <QuickActionButton>
-            <FontAwesomeIcon icon={['far', 'water']} />
-          </QuickActionButton>
-        </div>
-      ) : null}
-    </DeviceCard>
+    <Card className={cn('h-[280px]', className)}>
+      <CardHeader className="p-4">
+        <CardTitle className="z-40 flex w-fit items-center gap-2">
+          <span>Robot vacuum</span>
+        </CardTitle>
+        <p className="text-sm text-text-sub first-letter:capitalize">Docked</p>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center justify-between">
+        <Image
+          src="/images/vacuum/roborock_s8.webp"
+          alt="vacuum"
+          height={150}
+          width={150}
+          className="opacity-50"
+        />
+        <Button variant="outline" onClick={startVacuum}>
+          <TbPower className="mr-2" />
+          Start clearning
+        </Button>
+      </CardContent>
+    </Card>
   )
 }
